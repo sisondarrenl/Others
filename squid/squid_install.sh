51d08414095e4abc35b3023f25fc7aa6fdf2d31b
sisondarrenl <64946733+sisondarrenl@users.noreply.github.com>
2021-02-24 18:55:50 +0800

    Update squid_install.sh

--- squid/squid_install.sh
+++ squid/squid_install.sh
@@ -6,7 +6,7 @@ yum install squid -y
 
 #Configure ryslog.conf
 curl -LO https://raw.githubusercontent.com/sisondarrenl/Automation/main/squid/squid.conf
-mv rsyslog.conf /etc/squid/squid.conf
+mv squid.conf /etc/squid/squid.conf
 chmod 600 /etc/squid/squid.conf
                 
 #initialize rsyslog
